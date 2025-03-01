32cde727495cccc574d3fb15a915b47615e90b67
Jozeph-Zemambo <144367608+Jozeph-Zemambo@users.noreply.github.com>
2025-02-28 23:04:51 -0700

    Update DataUtilitiesTest.java

--- src/DataUtilitiesTest.java
+++ src/DataUtilitiesTest.java
@@ -13,6 +13,48 @@ import org.junit.Test;
 
 public class DataUtilitiesTest extends DataUtilities {
 
+	private Mockery mockingContext;
+	private Values2D values;
+    @BeforeClass public static void setUpBeforeClass() throws Exception {
+    }
+
+
+    @Before
+    public void setUp() throws Exception { 
+    	mockingContext = new Mockery();
+    	
+    	values = mockingContext.mock(Values2D.class);
+    	mockingContext.checking(new Expectations() {
+        {
+            one(values).getRowCount();
+            will(returnValue(2));
+            one(values).getColumnCount();
+            will(returnValue(3));
+            
+            one(values).getValue(0, 0);
+            will(returnValue(3));
+            one(values).getValue(0, 1);
+            will(returnValue(0));
+            one(values).getValue(0, -1);
+            will(returnValue(null));
+            one(values).getValue(1, -1);
+            will(returnValue(null));
+            one(values).getValue(0,3);
+            will(returnValue(null));
+            one(values).getValue(1,3);
+            will(returnValue(null));
+            one(values).getValue(0, 2);
+            will(returnValue(-5.5));
+            one(values).getValue(1, 0);
+            will(returnValue(5));
+            one(values).getValue(1, 1);
+            will(returnValue(0));
+            one(values).getValue(1, 2);
+            will(returnValue(-1));
+        }
+    });
+    }
+
     //Test cases for createNumberArray2D(double[][] data) method
     @Test
     //test for creating an invaild 2Darray of nulls
@@ -236,4 +278,61 @@ public class DataUtilitiesTest extends DataUtilities {
             fail("Exception thrown unexpectedly: " + e.getMessage());
         }
     }
+	
+//Test cases for CalculateColumnTotal method
+	
+// Test for null data
+    @Test
+    public void testCalculateColumnTotalWithNullData() {
+        try {
+            DataUtilities.calculateColumnTotal(null, 0);
+            fail("Expected an Exception to be thrown.");
+        } catch (Exception e) {
+            // Test passes, exception was thrown as expected
+        }
+    }
+
+    // Test for negative thus invalid index
+    @Test
+    public void testCalculateColumnTotalWithNegativeIndex() {
+        try {
+            DataUtilities.calculateColumnTotal(values, -1);
+            fail("Expected an Exception to be thrown.");
+        } catch (Exception e) {
+            // Test passes, exception was thrown as expected
+        }
+    }
+
+    // Test for out-of-scope thus invalid index
+    @Test
+    public void testCalculateColumnTotalWithOutOfScopeIndex() {
+        try {
+            DataUtilities.calculateColumnTotal(values, 3);
+            fail("Expected an Exception to be thrown.");
+        } catch (Exception e) {
+            // Test passes, exception was thrown as expected
+        }
+    }
+
+    // Test for two positive values
+    @Test
+    public void testCalculateColumnTotalForTwoPositiveValues() {
+        double result = DataUtilities.calculateColumnTotal(values, 0);
+        assertEquals(8.0, result, .000000001d);
+    }
+
+    // Test for two negative values
+    @Test
+    public void testCalculateColumnTotalForTwoNegativeValues() {
+        double result = DataUtilities.calculateColumnTotal(values, 1);
+        assertEquals(-6.5, result, .000000001d);
+    }
+
+    // Test for two zero values
+    @Test
+    public void testCalculateColumnTotalForTwoZeroValues() {
+        double result = DataUtilities.calculateColumnTotal(values, 1);
+        assertEquals(0.0, result, .000000001d);
+    }
 }
+
