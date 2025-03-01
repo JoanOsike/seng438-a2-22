d092096e846b7279bce7c7d70e9f64defbde67fc
Raghav <raghav.adlakha@ucalgary.ca>
2025-02-28 22:35:31 -0700

    Added Tests for UpperBound and LowerBound

--- src/RangeTest.java
+++ src/RangeTest.java
@@ -59,4 +59,78 @@ public class RangeTest {
     @AfterClass
     public static void tearDownAfterClass() throws Exception {
     }
+
+	//Tests for UpperBound
+
+	//Declaring and setting up variables
+	private Range finiteRangeUpper;
+    private Range infiniteRangeUpper;
+    private Range nanRangeUpper;
+
+    @Before
+    public void setUpUpper() throws Exception {
+        // Creating different range scenarios
+        finiteRangeUpper = new Range(-10.0, 15.5); // Finite range
+        infiniteRangeUpper = new Range(0.0, Double.POSITIVE_INFINITY); // Infinite upper bound
+        nanRangeUpper = new Range(5.0, Double.NaN); // NaN upper bound
+    }
+
+	//Test for checking upperbound when range has a finite range
+    @Test
+    public void testGetUpperBoundForFiniteValue() {
+        // Upper bound should be 15.5
+        assertEquals(15.5, finiteRangeUpper.getUpperBound(), 0.000000001d);
+    }
+
+	//test for checking upperbound when range has a positive infinity
+    @Test
+    public void testGetUpperBoundForInfiniteValue() {
+        // Upper bound should be POSITIVE_INFINITY
+        assertEquals(Double.POSITIVE_INFINITY, infiniteRangeUpper.getUpperBound(), 0.0);
+    }
+
+	//test for checking upperbound when range has a NaN
+    @Test
+    public void testGetUpperBoundForNaNValue() {
+        // Upper bound should be NaN
+        assertTrue(Double.isNaN(nanRangeUpper.getUpperBound()));
+    }
+
+
+	//Tests for LowerBound
+
+	//declaring and setting up variables
+	private Range finiteRangeLower;
+	private Range infiniteRangeLower;
+	private Range nanRangeLower;
+
+	@Before
+	public void setUpLower() throws Exception {
+		// Creating different range scenarios
+		finiteRangeLower = new Range(-10.0, 15.5); // Finite range
+		infiniteRangeLower = new Range(Double.NEGATIVE_INFINITY, 100.0); // Infinite lower bound
+		nanRangeLower = new Range(Double.NaN, 20.0); // NaN lower bound
+	}
+
+	//Test for checking lowerbound when range has a finite range
+	@Test
+	public void testGetLowerBoundForFiniteValue() {
+		// Lower bound should be -10.0
+		assertEquals(-10.0, finiteRangeLower.getLowerBound(), 0.000000001d);
+	}
+
+	//Test for checking lowerbound when range has a negative infinity 
+	@Test
+	public void testGetLowerBoundForInfiniteValue() {
+		// Lower bound should be NEGATIVE_INFINITY
+		assertEquals(Double.NEGATIVE_INFINITY, infiniteRangeLower.getLowerBound(), 0.0);
+	}
+
+	//Test for checking lowerbound when range has a NaN value
+	@Test
+	public void testGetLowerBoundForNaNValue() {
+		// Lower bound should be NaN
+		assertTrue(Double.isNaN(nanRangeLower.getLowerBound()));
+	}
+
 }
